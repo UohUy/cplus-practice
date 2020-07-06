935af1ceea7ada17767efbbc9cda33b7413bb66a
Administrator <houyu2006415@163.com>
2020-07-06 18:06:16 +1000

    Initial Files

---
+++ 1024/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << "This is from <iostream>" << endl;
+    printf("Hello World!\n");
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cplus-practice
+This is a personal C++ practice project which contains several individual tasks. The purpose of this project if for personal learning and practicing C++ programming skills. 
